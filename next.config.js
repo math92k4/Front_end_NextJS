9f211fbabd4f1c6d5f5ceab2abd0fe2d84f66d25
Mathias <math92k4@stud.kea.dk>
2022-11-14 13:40:37 +0100

    Day 1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["images.unsplash.com"],
 
-module.exports = nextConfig
+    // The default values
+    // imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+
+    imageSizes: [],
+    deviceSizes: [400, 500, 600, 700],
+    // the 2 arrays will be concatinated anyway,
+    // and deviceSizes enables that we can use vw.
+    // Therefore i think it's easier to just use "deviceSizes" - even though Next will throw a warning
+  },
+};
+
+module.exports = nextConfig;
